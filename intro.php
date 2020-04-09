38692617dfb494a04c6b58eab4a72fc51c764b11
anisanisa9 <55769863+anisanisa9@users.noreply.github.com>
2020-04-09 08:08:32 +0700

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,8 @@
+
+<?php
+    echo' HIII GUYS';
+    echo' Nama : Anisa <br>';
+    echo' Kelas : XI RPL 1 <br>';
+    echo' Asal Sekolah : SMK Taruna Bhakti <br>';
+    echo' Alamat Rumah : JL. Madrasah Rt01/07. No 148 <br>';
+?>
