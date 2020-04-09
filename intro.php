f5ea10917b257bb760b4c68de30e5eb99147ef40
anisanisa9 <55769863+anisanisa9@users.noreply.github.com>
2020-04-09 08:22:40 +0700

    add intro saya

--- intro.php
+++ intro.php
@@ -1,8 +1,8 @@
 
 <?php
-    echo' HIII GUYS';
+    echo' HIII GUYS <br>';
     echo' Nama : Anisa <br>';
     echo' Kelas : XI RPL 1 <br>';
-    echo' Asal Sekolah : SMK Taruna Bhakti <br>';
-    echo' Alamat Rumah : JL. Madrasah Rt01/07. No 148 <br>';
+    echo' Email : chacha0199@gmail.com <br>';
+    echo' No Telpon : 083806891628 <br>'; 
 ?>
